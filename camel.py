8ae3dae320567c944c47d96109491d0b8d4e9be8
Dovydas Gruzinskas <dg525@kent.ac.uk>
2026-02-02 13:18:19 +0000

    convert camel to snake

--- camel.py
+++ camel.py
@@ -1,1 +1,8 @@
-# Enter your code here
+word = input("Enter a word in camel case:")
+
+newWord = ''.join(['_' + char.lower() if char.isupper() else char for char in word])
+
+if newWord.startswith("_"):
+    newWord = newWord[1:]
+
+print(newWord)
